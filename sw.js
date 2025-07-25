c1b5b2b15ac6a93d7e4cd142d5eaf9cdc8f6fbbc
LcFitnessTraining <lcfitnesstrainingpt@gmail.com>
2025-07-25 17:54:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,12 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("lc-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+  console.log('Service Worker installato');
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  console.log('Service Worker attivato');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
